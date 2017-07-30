5dac75ea0855f8a8f5c75895982a28fcd4b6dbfa
dinukaroshan <dinuka.arseculeratne@genome.one>
2017-07-31 08:42:23 +1000

    adding base project

---
+++ src/main/java/com/quartz/mongo/intro/quartzintro/QuartzintroApplication.java
@@ -0,0 +1,12 @@
+package com.quartz.mongo.intro.quartzintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzintroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/quartz/mongo/intro/quartzintro/QuartzintroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quartz.mongo.intro.quartzintro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuartzintroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
